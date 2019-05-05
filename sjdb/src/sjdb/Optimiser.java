fd34a9cae7584faa647ef785cf6eb29b1f238d84
Diana Crintea <47833224+Dianna98@users.noreply.github.com>
2019-05-05 23:43:09 +0100

    Started working on optimiser

--- sjdb/src/sjdb/Optimiser.java
+++ sjdb/src/sjdb/Optimiser.java
@@ -1,11 +1,44 @@
 package sjdb;
 
-public class Optimiser {
-    public Optimiser(Catalogue catalogue) {
+import java.util.List;
 
+public class Optimiser implements PlanVisitor{
+
+    protected Catalogue cat;
+    protected Estimator estimator = new Estimator();
+
+    public Optimiser(Catalogue cat) {
+        this.cat = cat;
     }
 
     public Operator optimise(Operator plan) {
-        return plan;
+        plan.accept(this);
+        Operator optimisedPlan = null;
+        return optimisedPlan;
+    }
+
+    @Override
+    public void visit(Scan op) {
+
+    }
+
+    @Override
+    public void visit(Project op) {
+
+    }
+
+    @Override
+    public void visit(Select op) {
+
+    }
+
+    @Override
+    public void visit(Product op) {
+
+    }
+
+    @Override
+    public void visit(Join op) {
+
     }
 }
