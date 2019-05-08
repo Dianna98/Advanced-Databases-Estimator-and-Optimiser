c87dff66e9276fc9d8661ee8f81e0e3ee4aa0315
Diana Crintea <47833224+Dianna98@users.noreply.github.com>
2019-05-08 04:04:49 +0100

    Almost done

--- sjdb/src/sjdb/Optimiser.java
+++ sjdb/src/sjdb/Optimiser.java
@@ -6,243 +6,262 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class Optimiser implements PlanVisitor{
+public class Optimiser implements PlanVisitor {
 
-    public Catalogue cat;
-    public Estimator estimator = new Estimator();
-    public ArrayList<Attribute> attributes = new ArrayList<>();
-    public Set<Predicate> predicates = new HashSet<>();
-    public Set<Scan> scans = new HashSet<>();
+    private Catalogue cat;
+    private final Estimator estimator = new Estimator();
+    private Set<Attribute> attributes = new HashSet<>();
+    private Set<Predicate> predicates = new HashSet<>();
+    private Set<Scan> scans = new HashSet<>();
 
     public Optimiser(Catalogue cat) {
         this.cat = cat;
     }
 
-    public Operator optimise(Operator plan) {
-        System.out.println("Optimised:");
-        plan.accept(this);
-        List<Operator> operations = moveDown(plan,scans,predicates);
-        Operator optimisedPlan = optimalPlan(plan, operations, predicates);
-        return optimisedPlan;
-    }
-
-    @Override
     public void visit(Scan op) {
-        scans.add(op);
+        scans.add(new Scan((NamedRelation)op.getRelation()));
     }
 
-    @Override
     public void visit(Project op) {
         attributes.addAll(op.getAttributes());
     }
 
-    @Override
+    public void visit(Product op) {}
+
+    public void visit(Join op) {}
+
     public void visit(Select op) {
-        predicates.add(op.getPredicate());
-        attributes.add(op.getPredicate().getLeftAttribute());
-        if (!op.getPredicate().equalsValue()){
-            attributes.add(op.getPredicate().getRightAttribute());
+        Predicate p = op.getPredicate();
+        predicates.add(p);
+        attributes.add(p.getLeftAttribute());
+        if(!p.equalsValue()) attributes.add(p.getRightAttribute());
+    }
+
+    // this method returns the optimised query plan
+    public Operator optimise(Operator plan) {
+
+        plan.accept(this);
+
+        // move SELECTs and PROJECTS down the tree
+        List<Operator> operations = moveDown(plan,scans, predicates);
+
+        // choose the cheapest plan from the generated permutations
+
+        // return the optimised plan
+        return getOptimisedPlan(plan,predicates, operations);
+    }
+
+    // this method moves down the tree SELECT and PRODUCT operators
+    private List<Operator> moveDown(Operator root, Set<Scan> scans, Set<Predicate> predicates) {
+
+        List<Operator> operators = new ArrayList<>(scans.size());
+
+        for (Scan s: scans){
+            Operator o = createTopSelect(s, predicates);
+            List<Predicate> pred = new ArrayList<>(predicates);
+            operators.add(createTopProject(o, getAttributes(root,pred)));
         }
+
+        return operators;
     }
 
-    @Override
-    public void visit(Product op) {
+    // this method builds on the top level SELECT operators and returns the one that is closest to the root
+    private Operator createTopSelect(Operator op, Set<Predicate> predicates){
 
-    }
+        Operator result = op;
+        List<Attribute> attributes = result.getOutput().getAttributes();
 
-    @Override
-    public void visit(Join op) {
+        for (Predicate p : predicates){
 
-    }
-
-    public Operator optimalPlan(Operator op, List<Operator> operators, Set<Predicate> predicates){
-        Operator plan = null;
-        Integer minCost = Integer.MAX_VALUE;
-
-        List<Predicate> preds = new ArrayList<>(predicates);
-        List<List<Predicate>> permutations = permutations(preds);
-
-        for (List<Predicate> p : permutations){
-            List<Operator> ops = new ArrayList<>(operators);
-            Operator o = buildJoin(op,ops,p);
-
-            int cost = estimator.getCost(o);
-            if (cost<minCost){
-                minCost = cost;
-                plan = o;
+            if(result.getOutput() == null) {
+                result.accept(estimator);
             }
+
+            if ((p.equalsValue() && attributes.contains(p.getLeftAttribute())) ||
+                    (!p.equalsValue() && attributes.contains(p.getLeftAttribute()) && attributes.contains(p.getRightAttribute())))
+            {
+                result = new Select(result, p);
+                predicates.remove(p);
+            }
+
         }
 
-        System.out.println("The cost of the optimal solution is:    " +minCost );
-
-        return plan;
+        return result;
     }
 
-    private List<List<Predicate>> permutations(List<Predicate> predicates) {
-        List<List<Predicate>> result = new ArrayList<>();
+    // this method projects only the desired attributes and returns the PROJECT operator set as a root if not all attributes are wanted, or the old root operator otherwise
+    private Operator createTopProject(Operator op, Set<Attribute> attributes){
 
-        if (predicates.isEmpty()){
-            return  new ArrayList<>();
+        if(op.getOutput() == null) {
+            op.accept(estimator);
+        }
+
+        List<Attribute> attrs = new ArrayList<>(attributes);
+        attrs.retainAll(op.getOutput().getAttributes());
+
+        if (!attrs.isEmpty()) {
+            Operator o = new Project(op, attrs);
+            o.accept(estimator);
+            return o;
         } else {
-            Predicate fst = predicates.get(0);
-            predicates.remove(0);
-            List<List<Predicate>> perm = permutations(predicates);
-
-            for (List<Predicate> p : perm){
-                for (int i=0; i<p.size();i++) {
-                    ArrayList<Predicate> x = new ArrayList<>(p);
-                    x.add(i, fst);
-                    result.add(x);
-                }
-            }
+            return op;
         }
-        return result;
     }
 
-    private Operator buildJoin(Operator op, List<Operator> operators, List<Predicate> predicates) {
-        Operator result = null;
+    // this method returns the set of attributes desired, given an operator and a list of predicates
+    private Set<Attribute> getAttributes(Operator op, List<Predicate> predicates){
 
-        if (operators.size() == 1){
-            result = operators.get(0);
-            if (result.getOutput() == null){
-                result.accept(estimator);
-                return result;
-            }
-        }
-
-        for (Predicate p : predicates) {
-            Operator left = getOperator(operators, p.getLeftAttribute());
-            Operator right = getOperator(operators, p.getRightAttribute());
-
-            if ((left == null) ^ (right == null)) {
-                result = new Select(left != null ? left : right, p);
-                predicates.remove(p);
-            }
-
-            if (left != null && right != null) {
-                result = new Join(left, right, p);
-                predicates.remove(p);
-
-            }
-
-
-            if (result.getOutput() == null) {
-                result.accept(estimator);
-            }
-
-            Set<Attribute> attrs = getAttributes(op, predicates);
-
-            List<Attribute> available = result.getOutput().getAttributes();
-
-            if (attrs.size() == available.size() && available.containsAll(attrs)) {
-                operators.add(result);
-            } else {
-                List<Attribute> a = available.stream().filter(attrs::contains).collect(Collectors.toList());
-                if (a.size() == 0) {
-                    operators.add(result);
-                } else {
-                    Project proj = new Project(result, a);
-                    proj.accept(estimator);
-                    operators.add(proj);
-                }
-            }
-        }
-        
-        while (operators.size()>1){
-            Operator b1 = operators.get(0);
-            Operator b2 = operators.get(1);
-            Operator product = new Product(b1, b2);
-            product.accept(estimator);
-            operators.remove(b1);
-            operators.remove(b2);
-            operators.add(product);
-        }
-
-        result = operators.get(0);
-
-        return result;
-    }
-
-    private Operator getOperator(List<Operator> operators, Attribute attribute) {
-
-        for (Operator op : operators){
-            if (op.getOutput().getAttributes().contains(attribute)){
-                operators.remove(op);
-                return op;
-            }
-        }
-        return null;
-    }
-
-    public List<Operator> moveDown(Operator root, Set<Scan> scans, Set<Predicate> predicates){
-        List<Operator> plan = new ArrayList<>(scans.size());
-
-        for (Scan s : scans){
-            Operator op = selectTop(s,predicates);
-
-            List<Predicate> p = new ArrayList<>(predicates);
-            plan.add(projectTop(op, getAttributes(root,p)));
-        }
-
-        return plan;
-    }
-
-    private Set<Attribute> getAttributes(Operator op, List<Predicate> predicates) {
         Set<Attribute> attributes = new HashSet<>();
 
-        for (Predicate p : predicates) {
+        for(Predicate p : predicates){
             attributes.add(p.getLeftAttribute());
             if (!p.equalsValue()) {
                 attributes.add(p.getRightAttribute());
             }
         }
 
-        if (op instanceof Project){
-            attributes.addAll(((Project) op).getAttributes());
-        }
+        if (op instanceof Project) attributes.addAll(((Project) op).getAttributes());
 
         return attributes;
     }
 
-    private Operator selectTop(Operator op, Set<Predicate> predicates) {
-        Operator result = op;
-        //Relation rel = op.getOutput();
-        //List<Attribute> attributes = rel.getAttributes();
+    /* this method builds operators depending on the number of operators found for each predicate:
+     * if 0 operators found     -> builds a PRODUCT
+     * if 1 operator found      -> builds a SELECT
+     * if 2 operators found     -> builds a JOIN
+     * if more operators found  -> builds more PRODUCTs
+     */
+    private Operator build(Operator op, List<Operator> operators, List<Predicate> predicates){
 
-        List<Attribute> attributes = result.getOutput().getAttributes();
+        Operator result = null;
+        List<Predicate> preds = new ArrayList<>(predicates);
 
-        for (Predicate p : predicates){
-            if (result.getOutput() == null){
+        if (operators.size() == 1){
+            result = operators.get(0);
+            if (result.getOutput() == null) {
+                result.accept(estimator);
+            }
+            return result;
+        }
+        for(Predicate p : preds){
+            Operator left = getOperator(operators, p.getLeftAttribute());
+            Operator right = getOperator(operators, p.getRightAttribute());
+
+            // build SELECT
+            if(left == null ^ right == null){
+                result = new Select(left != null? left : right, p);
+                predicates.remove(p);
+            }
+
+            // build JOIN
+            if(left != null && right != null){
+                result = new Join(left, right, p);
+                predicates.remove(p);
+            }
+
+            if (result.getOutput() == null) {
                 result.accept(estimator);
             }
 
-            if (((!p.equalsValue() && attributes.contains(p.getLeftAttribute())) && attributes.contains(p.getRightAttribute())) || (attributes.contains(p.getLeftAttribute()) && p.equalsValue())){
-                result = new Select(result,p);
-                predicates.remove(p);
+            Set<Attribute> neededAttrs = getAttributes(op,preds);
+
+            List<Attribute> attributes = result.getOutput().getAttributes();
+
+            // build PROJECT
+            if (neededAttrs.size() == attributes.size() && attributes.containsAll(neededAttrs)){
+                operators.add(result);
+            }else{
+                List<Attribute> attrs = attributes.stream().filter(neededAttrs::contains).collect(Collectors.toList());
+
+                if (attrs.size() == 0) {
+                    operators.add(result);
+                } else {
+                    Project project = new Project(result, attrs);
+                    project.accept(estimator);
+                    operators.add(project);
+                }
             }
         }
+
+        // build more PRODUCTs
+        while(operators.size()>1) {
+            Operator o1 = operators.get(0);
+            Operator o2 = operators.get(1);
+            Operator product = new Product(o1, o2);
+            product.accept(estimator);
+
+            operators.remove(o1);
+            operators.remove(o2);
+            operators.add(product);
+        }
+
+        // return the root
+        result = operators.get(0);
+
         return result;
     }
 
-    private Operator projectTop(Operator op, Set<Attribute> attributes) {
+    // this method returns the first operator in the list which contains the given attribute
+    private Operator getOperator(List<Operator> operators, Attribute attribute){
 
-        //Relation rel = op.getOutput();
-        List<Attribute> attrs = new ArrayList<>(attributes);
+        List<Operator> ops = new ArrayList<>(operators);
 
-        if (op.getOutput()==null){
-       // if (rel == null){
-            op.accept(estimator);
+        for (Operator o : ops){
+            if (o.getOutput().getAttributes().contains(attribute)){
+                operators.remove(o);
+                return o;
+            }
+        }
+        return null;
+    }
+
+    // this method finds the cheapest query plan from all n! permutations
+    private Operator getOptimisedPlan(Operator op, Set<Predicate> predicates, List<Operator> operators){
+
+        Operator cheapest = null;
+        int minCost = Integer.MAX_VALUE;
+        List<Predicate> preds = new ArrayList<>(predicates);
+        List<List<Predicate>> permutations = permutations(preds);
+
+        for (List<Predicate> p : permutations) {
+            List<Operator> ops = new ArrayList<>(operators);
+
+            Operator plan = build(op,ops, p);
+
+            int cost = estimator.getCost(plan);
+            if (cost<minCost){
+                minCost = cost;
+                cheapest = plan;
+            }
         }
 
-        attrs.retainAll(op.getOutput().getAttributes());
+        System.out.println("The cost of the optimal query plan is:  "+ minCost);
 
-        if (!attrs.isEmpty()){
-            Operator o = new Project(op, attrs);
-            o.accept(estimator);
-            return o;
+        return cheapest;
+    }
+
+    // this method generates a list with all permutations possible, given a list of predicates
+    private List<List<Predicate>> permutations(List<Predicate> predicates) {
+
+        if (predicates.isEmpty()) {
+            List<List<Predicate>> result = new ArrayList<>();
+            result.add(new ArrayList<>());
+            return result;
         }
 
-        return op;
+        Predicate fst = predicates.remove(0);
+        List<List<Predicate>> list = new ArrayList<>();
+        List<List<Predicate>> permutations = permutations(predicates);
+
+        for (List<Predicate> partial : permutations) {
+            for (int i=0; i <= partial.size(); i++) {
+                List<Predicate> part = new ArrayList<>(partial);
+                part.add(i, fst);
+                list.add(part);
+            }
+        }
+
+        return list;
     }
 
 }
